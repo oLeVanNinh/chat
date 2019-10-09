69870d4e893a54a9cdd661e695d360d277e58499
cloud <ninhle.9984@gmail.com>
2019-10-10 00:05:04 +0700

    Add client login UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
+import { CustomMaterialModule } from "./core/material.module";
+import { AppRoutingModule } from "./core/app.routing.module";
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/core/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule, MatToolbarModule, MatMenuModule, MatIconModule,
+  MatProgressSpinnerModule } from "@angular/material";
+
+@NgModule({
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule, MatToolbarModule, MatMenuModule, MatIconModule,
+    MatProgressSpinnerModule ],
+  exports: [CommonModule, MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule, MatToolbarModule, MatMenuModule, MatIconModule,
+    MatProgressSpinnerModule ],
+})
+
+export class CustomMaterialModule {}
